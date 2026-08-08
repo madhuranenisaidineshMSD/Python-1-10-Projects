708890229ffd03fbb606319a5a405a675954ed00
madhuranenisaidineshMSD <infinitemsd@gmail.com>
2026-08-08 23:38:05 +0530

    some new code

---
+++ Countdown Timer/Countdown Timer.py
@@ -0,0 +1,16 @@
+import time
+
+
+def countdown_timer(t):
+
+    while t > 0 :
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+
+    print("Time's up!")
+
+t = input("Enter the time in seconds: ")
+countdown_timer(int(t))
